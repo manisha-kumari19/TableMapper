536595f26c2f97283b3bbb7101d67cd8dedc287a
manisha-kumari19 <mkumari@argusoft.com>
2024-01-08 16:58:26 +0530

    initial commit

---
+++ backend/src/test/java/com/TableMapper/TableMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TableMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
