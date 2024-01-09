d57f292d6d832c3edce948254917a0b74ede89c3
manisha-kumari19 <mkumari@argusoft.com>
2024-01-09 15:27:58 +0530

    set structure

---
+++ backend/src/main/java/com/TableMapper/Dto/JsonData.java
@@ -0,0 +1,34 @@
+package com.TableMapper.Dto;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class JsonData {
+   private String tableName;
+   private String parentTable;
+
+
+    Map<String, Object> fields = new LinkedHashMap<>();
+
+    @JsonSetter
+    void setFields(String key, Object value) {
+        fields.put(key, value);
+    }
+
+    @JsonGetter
+    public Map<String, Object> getFields() {
+        return fields;
+    }
+
+}
